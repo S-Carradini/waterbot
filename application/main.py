16659c59b3848b9d9e95a98b3da26d46dea9e3fc
Jacob Kuriakose <jacobjk03@gmail.com>
2025-10-06 16:34:51 -0700

    Fix: isolate user sessions by removing shared UUID logic in SetCookieMiddleware

--- application/main.py
+++ application/main.py
@@ -76,8 +76,8 @@ class SetCookieMiddleware(BaseHTTPMiddleware):
     
     async def dispatch(self, request: Request, call_next):
         # store session ID in memory if we are in situation where client has cookies disabled.
-        session_value = request.cookies.get(COOKIE_NAME) or self.client_cookie_disabled_uuid or str(uuid.uuid4())
-        self.client_cookie_disabled_uuid=session_value
+        session_value = request.cookies.get(COOKIE_NAME) or str(uuid.uuid4())
+        # self.client_cookie_disabled_uuid=session_value
     
         request.state.client_cookie_disabled_uuid = "COOKIE_DISABLED."+session_value
 
