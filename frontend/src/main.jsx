3dc3d4ab1deee4ae73997f0f287173ade0fea217
Ram Harikrishnan <shankerram3@gmail.com>
2025-12-05 05:50:55 -0700

    Add splash screen as root page with routing and fix video aspect ratio
    - Set up React Router with splash screen as root (/) and chatbot at /waterbot
    - Install react-router-dom dependency
    - Fix video background aspect ratio using object-fit: cover
    - Splash screen button navigates to chatbot page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
+import SplashScreen from './components/SplashScreen'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SplashScreen />} />
+        <Route path="/waterbot" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
 
